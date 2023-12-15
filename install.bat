fcea80da5eff194b5443446ee84020e5259ff4d4
Danil Boldyrev <daswerq123@gmail.com>
2023-12-15 19:52:31 +0300

    update deepspeed to 11.2

--- install.bat
+++ install.bat
@@ -5,7 +5,9 @@ call venv/scripts/activate
 
 pip install torch==2.1.1+cu118 torchaudio==2.1.1+cu118 --index-url https://download.pytorch.org/whl/cu118
 pip install -r requirements.txt
-pip install deepspeed/deepspeed-0.12.4+unknown-py3-none-any.whl
+
+@echo Instaling deepspeed for python 3.10.x and CUDA 11.8
+pip install deepspeed/deepspeed-0.11.2+unknown-cp310-cp310-win_amd64.whl
 
 @echo Install complete
 pause
