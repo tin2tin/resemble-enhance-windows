fddfd9bc51bb4835792d4483e35672deddc18633
Danil Boldyrev <daswerq123@gmail.com>
2023-12-16 21:02:27 +0300

    Optimize VRAM utilization

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ def clear_gpu_cash():
     if torch.cuda.is_available():
         torch.cuda.empty_cache()
 
-def _fn(path, solver, nfe, tau, denoising):
+def _fn(path, solver, nfe, tau,chunk_seconds,chunks_overlap, denoising):
     if path is None:
         return None, None
 
@@ -29,7 +29,7 @@ def _fn(path, solver, nfe, tau, denoising):
     dwav = dwav.mean(dim=0)
 
     wav1, new_sr = denoise(dwav, sr, device)
-    wav2, new_sr = enhance(dwav, sr, device, nfe=nfe, solver=solver, lambd=lambd, tau=tau)
+    wav2, new_sr = enhance(dwav = dwav, sr = sr, device = device, nfe=nfe,chunk_seconds=chunk_seconds,chunks_overlap=chunks_overlap, solver=solver, lambd=lambd, tau=tau)
 
     wav1 = wav1.cpu().numpy()
     wav2 = wav2.cpu().numpy()
@@ -44,6 +44,9 @@ def main():
         gr.Dropdown(choices=["Midpoint", "RK4", "Euler"], value="Midpoint", label="CFM ODE Solver"),
         gr.Slider(minimum=1, maximum=128, value=64, step=1, label="CFM Number of Function Evaluations"),
         gr.Slider(minimum=0, maximum=1, value=0.5, step=0.01, label="CFM Prior Temperature"),
+        gr.Slider(minimum=1, maximum=40, value=10, step=1, label="Chunk seconds (more secods more VRAM usage)"),
+        gr.Slider(minimum=0, maximum=5, value=1, step=0.5, label="Chunk overlap"),
+        # chunk_seconds, chunks_overlap
         gr.Checkbox(value=False, label="Denoise Before Enhancement"),
     ]
 
