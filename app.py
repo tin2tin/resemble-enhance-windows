cf2ec31035a01d61af7ab5823b26a9b55fb9282d
Danil Boldyrev <daswerq123@gmail.com>
2023-12-21 04:07:52 +0300

    Add more information about options, remove lock for windows

--- app.py
+++ app.py
@@ -41,13 +41,13 @@ def _fn(path, solver, nfe, tau,chunk_seconds,chunks_overlap, denoising):
 def main():
     inputs: list = [
         gr.Audio(type="filepath", label="Input Audio"),
-        gr.Dropdown(choices=["Midpoint", "RK4", "Euler"], value="Midpoint", label="CFM ODE Solver"),
-        gr.Slider(minimum=1, maximum=128, value=64, step=1, label="CFM Number of Function Evaluations"),
-        gr.Slider(minimum=0, maximum=1, value=0.5, step=0.01, label="CFM Prior Temperature"),
+        gr.Dropdown(choices=["Midpoint", "RK4", "Euler"], value="Midpoint", label="CFM ODE Solver (Midpoint is recommended)"),
+        gr.Slider(minimum=1, maximum=128, value=64, step=1, label="CFM Number of Function Evaluations (higher values in general yield better quality but may be slower)"),
+        gr.Slider(minimum=0, maximum=1, value=0.5, step=0.01, label="CFM Prior Temperature (higher values can improve quality but can reduce stability)"),
         gr.Slider(minimum=1, maximum=40, value=10, step=1, label="Chunk seconds (more secods more VRAM usage)"),
         gr.Slider(minimum=0, maximum=5, value=1, step=0.5, label="Chunk overlap"),
         # chunk_seconds, chunks_overlap
-        gr.Checkbox(value=False, label="Denoise Before Enhancement"),
+        gr.Checkbox(value=False, label="Denoise Before Enhancement (tick if your audio contains heavy background noise)"),
     ]
 
     outputs: list = [
--- install.bat
+++ install.bat
@@ -3,8 +3,8 @@
 python -m venv venv
 call venv/scripts/activate
 
-pip install torch==2.1.1+cu118 torchaudio==2.1.1+cu118 --index-url https://download.pytorch.org/whl/cu118
 pip install -r requirements.txt
+pip install torch==2.1.1+cu118 torchaudio==2.1.1+cu118 --index-url https://download.pytorch.org/whl/cu118
 
 @echo Instaling deepspeed for python 3.10.x and CUDA 11.8
 pip install deepspeed/deepspeed-0.11.2+cuda118-cp310-cp310-win_amd64.whl
